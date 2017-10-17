a8d9f4dee5030643785a4015300990256e64b2d8
yeticek <kysel.petrik1@gmail.com>
2017-10-17 20:08:37 +0200

    menu selectors

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,7 +1,19 @@
-$(function() {
-  $(".nav li").on("click", function() {
-    $(".nav li").removeClass("active");
-    $(this).addClass("active");
-  });
-  
+$(document).ready(function ($) {
+    var url = window.location.href;
+    var activePage = url;
+    $('.irp-menu-item a').each(function () {
+        var linkPage = this.href;
+
+        if (activePage == linkPage) {
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
+
+$(function() { 
+    $('#navbar').on('click','.irp-menu-item', function ( e ) {
+        e.preventDefault();
+        $(this).parents('#navbar').find('.active').removeClass('active').end().end().addClass('active');
+        $(activeTab).show();
+    });
 });
