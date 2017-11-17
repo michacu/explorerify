fa70e5774ca6dd1ceacf936e2fdc6c704491f8e2
michacu <michacu789@gmail.com>
2017-11-17 18:25:46 +0100

    contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, :only => [:show]
   get 'index/index'
-
+  get 'contact/show'
   root 'index#index'
 end
