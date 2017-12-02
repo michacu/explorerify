33be2436e9a65101bc27060fb0a7e35699e3b827
michacu <michacu789@gmail.com>
2017-12-02 19:59:58 +0100

    user add avatar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
-    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :nickname, :past_countries, :want_countries,
+     :about, :last_name, :email, :password, :password_confirmation, :avatar_url)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :nickname, :past_countries, :want_countries, :about, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :nickname, :past_countries, :want_countries,
+     :about, :last_name, :email, :password, :password_confirmation, :current_password, :avatar_url)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202143536) do
+ActiveRecord::Schema.define(version: 20171202174328) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171202143536) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "avatar_url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
